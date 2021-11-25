858ef31833a5708cf6691f57b5e3590e664d613c
Felipe Garzón Castro <afgarzonc@gmail.com>
2021-11-25 08:00:53 -0500

    react base project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const hi = (
+  <p>
+    Hola <b>Mundo</b>
+  </p>
+);
+
+const inicio = document.getElementById("root");
+
+ReactDOM.render(hi, inicio);
